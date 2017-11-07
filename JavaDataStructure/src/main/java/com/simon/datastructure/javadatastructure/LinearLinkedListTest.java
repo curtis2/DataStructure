4caca3211d86e35a06a89782f0ccea42d7915dfe
curtis2 <18292967668@163.com>
2017-11-07 18:19:28 +0800

    增加链表相关测试代码

---
+++ JavaDataStructure/src/main/java/com/simon/datastructure/javadatastructure/LinearLinkedListTest.java
@@ -0,0 +1,12 @@
+package com.simon.datastructure.javadatastructure;
+
+/**
+ * auther: Simon zhang
+ * Emaill:18292967668@163.com
+ */
+
+public class LinearLinkedListTest {
+    public static void main(String[] args){
+
+    }
+}
